19edea4f16ad7f345fc9413fad53daefc004e1f1
MATRON_NODE5 <MATRON_NODE5@shlee>
2019-10-23 15:54:24 +0900

    first commit

---
+++ HELLO/src/Test.java
@@ -0,0 +1,25 @@
+
+public class Test {
+
+	
+	int aa;
+	float bb;
+	
+	
+	Test()
+	{
+		aa = 5;
+		bb = 10.5f;		
+	}
+	
+	
+	public void print_aa()
+	{
+		System.out.println("aa = "+ aa);
+	}
+	
+	public void print_bb()
+	{
+		System.out.println("bb = "+ bb);
+	}
+}
---
+++ HELLO/src/helloworld.java
@@ -0,0 +1,14 @@
+
+public class helloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Test test = new Test();
+		
+		test.print_aa();
+		test.print_bb();
+
+	}
+
+}
