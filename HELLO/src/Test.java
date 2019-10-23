33d47c26f59a86e83fe747a566774a1f971a15c4
MATRON_NODE5 <MATRON_NODE5@shlee>
2019-10-23 16:11:40 +0900

    4

---
+++ HELLO/src/Test.java
@@ -0,0 +1,50 @@
+
+public class Test {
+
+	
+	int aa;
+	float bb;
+	
+	
+	Test()
+	{
+		aa = 5;
+		bb = 10.5f;		
+	}
+	
+	
+	public void print_aa()
+	{
+		System.out.println("aa = "+ aa);
+	}
+	
+	public void print_bb()
+	{
+		System.out.println("bb = "+ bb);
+	}
+	
+	public void print_cc()
+	{
+		System.out.println("bb = "+ bb);
+	}
+	
+	public void print_dd()
+	{
+		System.out.println("bb = "+ bb);
+	}
+	
+	public void print_ee()
+	{
+		System.out.println("bb = "+ bb);
+	}
+	
+	public void print_ff()
+	{
+		System.out.println("bb = "+ bb);
+	}
+	
+	public void print_gg()
+	{
+		System.out.println("bb = "+ bb);
+	}
+}
